1b53d937bb507f6d6bfcff30882cd74e77ea03b8
syt <ununian@outlook.com>
2021-03-27 15:30:43 +0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true, // 未尾逗号
+  vueIndentScriptAndStyle: false,
+  singleQuote: true, // 单引号
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  trailingComma: false, // 未尾分号
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  arrowParens: 'always',
+  insertPragma: false,
+  requirePragma: false,
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'lf'
+}
